692e67bb1a2a34d469ef68693dab64ee663f8645
Mate Gujgiczer <mate.gujgiczer@icloud.com>
2016-02-29 16:08:18 +0100

    - initial commit

---
+++ EventEmitter.swift
@@ -0,0 +1,91 @@
+//
+//  EventEmitter.swift
+//  Mooovy
+//
+//  Created by Gujgiczer Máté on 08/02/16.
+//  Ispired by Balázs Schwarzkopf
+//  Copyright © 2016 Runninglobe. All rights reserved.
+//
+
+import Foundation
+
+struct EventListenerAction <T> {
+    var listenerAction : ((T?) -> ())
+    
+    init(callback:((T?) -> ())) {
+        listenerAction = callback;
+    }
+    
+    init(callback:(() -> ())) {
+        listenerAction = {(data :T?) -> () in
+            callback()
+        }
+    }
+}
+
+public protocol EventEmitter {
+    var listeners : Dictionary<String, Array<Any>>? {get set}
+    
+    /// Create a new event listener, not expecting information from the trigger
+    /// - Parameters:
+    ///     - eventName: Matching trigger eventNames will cause this listener to fire
+    ///     - action: The block of code you want executed when the event triggers
+    mutating func on(eventName:String, action:(()->()))
+    
+    /// Create a new event listener, expecting information from the trigger
+    /// - Parameters:
+    ///     - eventName: Matching trigger eventNames will cause this listener to fire
+    ///     - action: The block of code you want executed when the event triggers
+    mutating func on<T>(eventName:String, action:((T?)->()))
+    
+    /// Triggers an event
+    /// - Parameters:
+    ///     - eventName: Matching listener eventNames will fire when this is called
+    ///     - information: pass values to your listeners
+    func emit(eventName:String, information:Any?)
+    
+    /// Removes all listeners by default, or specific listeners through paramters
+    /// - Parameters:
+    ///     - eventName: If an event name is passed, only listeners for that event will be removed
+    mutating func removeListeners(eventNameToRemoveOrNil:String?)
+}
+
+extension EventEmitter {
+    mutating func on(eventName:String, action:(()->())) {
+        let newListener = EventListenerAction<Any>(callback: action);
+        addListener(eventName, newEventListener: newListener)
+    }
+
+    mutating func on<T>(eventName:String, action:((T?)->())) {
+        let newListener = EventListenerAction(callback: action);
+        addListener(eventName, newEventListener: newListener)
+    }
+
+    mutating private func addListener<T>(eventName:String, newEventListener:EventListenerAction<T>) {
+        if var listenerArray = self.listeners?[eventName] {
+            listenerArray.append(newEventListener);
+        }
+        else {
+            self.listeners?[eventName] = [newEventListener];
+        }
+    }
+    
+    mutating func removeListeners(eventNameToRemoveOrNil:String?) {
+        if let eventNameToRemove = eventNameToRemoveOrNil {
+            if var actionArray = self.listeners?[eventNameToRemove] {
+                actionArray.removeAll();
+            }
+        }
+        else {
+            self.listeners?.removeAll(keepCapacity: false);
+        }
+    }
+    
+    func emit(eventName:String, information:Any? = nil) {
+        if let actionObjects = self.listeners?[eventName] {
+            actionObjects.forEach() {
+                ($0 as? EventListenerAction)?.listenerAction(information)
+            }
+        }
+    }
+}
